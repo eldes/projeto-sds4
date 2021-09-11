c5850b3ca44240c75607f03a104245e691a2f5b5
Eldes <studio@eldes.com>
2021-09-11 15:02:24 -0300

    Home, navigation

--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -1,9 +1,19 @@
 import Page from 'components/Page'
 import { FunctionComponent } from 'react'
+import { Link } from 'react-router-dom'
 
 const HomePage: FunctionComponent = () => {
 	return (
-		<Page title="Home" />
+		<Page title="DSVendas">
+			<div className="container">
+				<div className="jumbotron">
+					<p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+					<hr/>
+					<p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+					<Link to="/dashboard" className="btn btn-primary btn-large">Acessar o Dashboard</Link>
+				</div>
+			</div>
+		</Page>
 	)
 }
 
